8eb1aa3cf9cdbfc9fe646868a63645ac8fd7922f
Raksit Mantanacharu <raksit.m@ku.th>
2018-04-22 23:55:46 +0700

    changed weapon kill data

--- app/profile.js
+++ app/profile.js
@@ -4,57 +4,66 @@ const { Router } = require('express')
 const router = Router()
 const API = require('./constants')
 
-let getJoinedMatches = (userID, matches) => {
-    return matches.map(match => {
-        if (_.includes(match.players, userID)) return match
+let getRecentMatches = (userID, matches) => {
+    return _.filter(matches, match => {
+        return _.includes(match.players, userID)
     })
 }
 
 let getNumberWins = (userID, matches) => {
-    return _.size(matches.map(match => {
+    return _.size(_.filter(matches, match => {
         return match.winner === userID
     }))
 }
 
-let getNumberKills = (userID, kills) => {
-    return _.size(kills.map(kill => {
+let getKills = (userID, kills) => {
+    let ownKills = _.filter(kills, kill => {
         return kill.playerID === userID && kill.victimID !== userID
-    }))
+    })
+
+    let weapons = _.countBy(ownKills, 'weaponUsed')
+    return _.map(Object.keys(weapons), weapon => {
+        return {
+            name: weapon,
+            kills: weapons[weapon]
+        }
+    })
 }
 
 router.get('/:user_id', (req, res) => {
     let userID = req.params.user_id
-    
+
     axios.get(API.USER + `/u/${userID}`).then(userRes => {
         axios.get(API.MATCH).then(matchRes => {
             axios.get(API.KILL).then(killRes => {
                 let user = userRes.data
                 let matches = matchRes.data
                 let kills = killRes.data
+                let recentMatches = getRecentMatches(userID, matches)
 
                 let result = {
-                username: user.username,
-                score: user.score,
-                clothIndex: user.clothIndex,
-                joinedMatches: getJoinedMatches(userID, matches),
-                numWins: getNumberWins(userID, matches),
-                numKills: getNumberKills(userID, kills)
-            }
+                    username: user.username,
+                    score: user.score,
+                    clothIndex: user.clothIndex,
+                    recentMatches: recentMatches,
+                    kills: getKills(userID, kills),
+                    numWins: getNumberWins(userID, recentMatches),
+                }
 
-            return res.status(200).send(result)
+                return res.status(200).send(result)
 
             })
+                .catch(err => {
+                    return res.status(500, 'Something is wrong with KILL API ?')
+                })
+        })
             .catch(err => {
-                return res.status(500, 'Something is wrong with KILL API ?')
+                return res.status(500, 'Something is wrong with MATCH API ?')
             })
-        })
+    })
         .catch(err => {
-            return res.status(500, 'Something is wrong with MATCH API ?')
+            return res.status(500, 'Something is wrong with USER API ?')
         })
-    })
-    .catch(err => {
-        return res.status(500, 'Something is wrong with USER API ?')
-    })
 })
 
 module.exports = router
